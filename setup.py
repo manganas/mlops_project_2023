4fe1eb3585315665589a231c47a328b042b1c6a6
manganas <manganas.ioannis@gmail.com>
2023-01-06 11:15:25 +0100

    New cookiecutter project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Bird species classification project for the course Machine Learning Operations at DTU.',
+    author='team_31',
+    license='MIT',
+)
