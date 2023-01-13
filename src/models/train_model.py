1968437ae276a8b1b9ea0bd18a20c1e8ca4d775e
manganas <manganas.ioannis@gmail.com>
2023-01-13 16:05:26 +0100

    fixed feat_extr cache

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,31 +1,31 @@
-from transformers import AutoFeatureExtractor
-from transformers import AutoModelForImageClassification, TrainingArguments, Trainer
+import logging
 
-from datasets import load_metric
-
-
-import torch
-from torchvision.transforms import (
-    ToTensor,
-    Normalize,
-    RandomResizedCrop,
-    CenterCrop,
-    RandomHorizontalFlip,
-    Resize,
-    Compose,
-)
-
-from src.data.make_dataset import BirdsDataset
+from typing import Dict
 
 import hydra
-from hydra.core.config_store import ConfigStore
-from src.config import BirdsConfig
-
 import numpy as np
+import torch
+from datasets import load_metric
+from hydra.core.config_store import ConfigStore
+from torch.utils.data import DataLoader
 
+
+from tqdm import tqdm
+from transformers import (
+    AutoFeatureExtractor,
+    get_scheduler,
+)
+
+from src.config import BirdsConfig
+from src.data.make_dataset import BirdsDataset
+from src.models.model import MyClassifier
 
 cs = ConfigStore.instance()
 cs.store("birds_config", node=BirdsConfig)
+
+
+def move_to(x, device: torch.device):
+    pass
 
 
 @hydra.main(config_path="../conf", config_name="config.yaml")
@@ -48,144 +48,105 @@ def main(cfg: BirdsConfig):
     lr = cfg.hyperparameters.lr
     batch_size = cfg.hyperparameters.batch_size
     epochs = cfg.hyperparameters.epochs
+    gpu = cfg.hyperparameters.gpu
 
     #############
     #############
+
+    device = "cuda" if (gpu and torch.cuda.is_available()) else "cpu"
+    print(f"Device: {device}")
+    device = torch.device(device)
 
     ## Logic that checks if an already trained model exists!
 
-    feature_extractor = AutoFeatureExtractor.from_pretrained(pretrained_model)
+    feature_extractor = AutoFeatureExtractor.from_pretrained(
+        pretrained_model, cache_dir=feature_extractor_cache
+    )
 
     # Prepare datasets
-    train_dataset = BirdsDataset(
+    train_data = BirdsDataset(
         input_filepath=data_input_filepath,
         output_filepath=data_output_filepath,
         data_type="train",
         feature_extractor=feature_extractor,
-    ).get_data()
+    )
 
-    valid_dataset = BirdsDataset(
+    train_dataset = train_data.get_data()
+
+    valid_data = BirdsDataset(
         input_filepath=data_input_filepath,
         output_filepath=data_output_filepath,
         data_type="valid",
         feature_extractor=feature_extractor,
-    ).get_data()
-
-    labels = train_dataset.features["label"].names
-    label2id, id2label = dict(), dict()
-    for i, label in enumerate(labels):
-        label2id[label] = i
-        id2label[i] = label
-
-        feature_extractor = AutoFeatureExtractor.from_pretrained(
-            pretrained_model, cache_dir=feature_extractor_cache
-        )
-
-    normalize = Normalize(
-        mean=feature_extractor.image_mean, std=feature_extractor.image_std
     )
 
-    train_transforms = Compose(
-        [
-            RandomResizedCrop(feature_extractor.size),
-            RandomHorizontalFlip(),
-            ToTensor(),
-            normalize,
-        ]
-    )
+    valid_dataset = valid_data.get_data()
 
-    val_transforms = Compose(
-        [
-            Resize(feature_extractor.size),
-            CenterCrop(feature_extractor.size),
-            ToTensor(),
-            normalize,
-        ]
-    )
+    # Get features from the data
 
-    def preprocess_train(example_batch):
-        """Apply train_transforms across a batch."""
-        example_batch["pixel_values"] = [
-            train_transforms(image.convert("RGB")) for image in example_batch["image"]
-        ]
-        return example_batch
+    def tokenize_function(examples) -> Dict:
+        return feature_extractor(examples["image"])
 
-    def preprocess_val(example_batch):
-        """Apply val_transforms across a batch."""
-        example_batch["pixel_values"] = [
-            val_transforms(image.convert("RGB")) for image in example_batch["image"]
-        ]
-        return example_batch
+    train_dataset = train_dataset.map(tokenize_function, batched=True)
+    train_dataset = train_dataset.remove_columns(["image"])
+    train_dataset = train_dataset.rename_column("label", "labels")
+    train_dataset.set_format("torch")
 
-    train_dataset.set_transform(preprocess_train)
-    valid_dataset.set_transform(preprocess_val)
+    valid_dataset = valid_dataset.map(tokenize_function, batched=True)
+    valid_dataset = valid_dataset.remove_columns(["image"])
+    valid_dataset = valid_dataset.rename_column("label", "labels")
+    valid_dataset.set_format("torch")
 
-    model = AutoModelForImageClassification.from_pretrained(
-        pretrained_model,
-        label2id=label2id,
-        id2label=id2label,
-        ignore_mismatched_sizes=True,  # provide this in case you're planning to fine-tune an already fine-tuned checkpoint
-        cache_dir=feature_extractor_cache,
-    )
+    train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size)
+    valid_dataloader = DataLoader(valid_dataset, shuffle=False, batch_size=batch_size)
+
+    model_options = {"ignore_mismatched_sizes": True}
+
+    model = MyClassifier(
+        pretrained_model=pretrained_model,
+        num_labels=train_data.num_classes,
+        feature_extractor_cache=feature_extractor_cache,
+        **model_options,
+    ).get_model()
 
     model_name = pretrained_model.split("/")[-1]
 
-    args = TrainingArguments(
-        f"{model_name}-finetuned",
-        remove_unused_columns=False,
-        evaluation_strategy="epoch",
-        save_strategy="epoch",
-        learning_rate=lr,
-        per_device_train_batch_size=batch_size,
-        gradient_accumulation_steps=4,
-        per_device_eval_batch_size=batch_size,
-        num_train_epochs=epochs,
-        warmup_ratio=0.1,
-        logging_steps=10,
-        load_best_model_at_end=True,
-        metric_for_best_model="accuracy",
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+
+    num_training_steps = epochs * len(train_dataloader)
+    lr_scheduler = get_scheduler(
+        name="linear",
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=num_training_steps,
     )
 
-    metric = load_metric("accuracy")
+    model.to(device)
 
-    # the compute_metrics function takes a Named Tuple as input:
-    # predictions, which are the logits of the model as Numpy arrays,
-    # and label_ids, which are the ground-truth labels as Numpy arrays.
-    def compute_metrics(eval_pred):
-        """Computes accuracy on a batch of predictions"""
-        predictions = np.argmax(eval_pred.predictions, axis=1)
-        return metric.compute(predictions=predictions, references=eval_pred.label_ids)
+    for epoch in tqdm(range(epochs), desc="Training"):
+        model.train()
+        running_loss = 0.0
 
-    def collate_fn(examples):
-        pixel_values = torch.stack([example["pixel_values"] for example in examples])
-        labels = torch.tensor([example["label"] for example in examples])
-        return {"pixel_values": pixel_values, "labels": labels}
+        for batch in tqdm(train_dataloader, desc="Batch", leave=False):
 
-    trainer = Trainer(
-        model,
-        args,
-        train_dataset=train_dataset,
-        eval_dataset=valid_dataset,
-        tokenizer=feature_extractor,
-        compute_metrics=compute_metrics,
-        data_collator=collate_fn,
-    )
+            optimizer.zero_grad()
 
-    train_results = trainer.train()
-    # rest is optional but nice to have
-    trainer.save_model(output_dir=saved_models_dir)
-    trainer.log_metrics("train", train_results.metrics)
-    trainer.save_metrics(
-        "train",
-        train_results.metrics,
-    )
-    trainer.save_state(output_dir=saved_weights_dir)
+            batch = {k: v.to(device) for k, v in batch.items()}
 
-    metrics = trainer.evaluate()
-    # some nice to haves:
-    trainer.log_metrics("eval", metrics)
-    trainer.save_metrics("eval", metrics)
+            y_pred = model(**batch)
+
+            loss = y_pred.loss
+
+            loss.backward()
+
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            lr_scheduler.step()
 
 
 if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+    logger.info("training model")
     main()
